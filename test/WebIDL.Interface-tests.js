d7d9edd7e346221b8f452c9fd51e81e1bf548344
clintjhill <clint.hill@gmail.com>
2013-02-17 15:03:10 -0700

    Better comments on implementsInterface function.

--- test/WebIDL.Interface-tests.js
+++ test/WebIDL.Interface-tests.js
@@ -44,8 +44,8 @@ require(["types/Interface"], function(Interface) {
 
     requirement = "If V is a user object that is considered to implement I according to the rules in section 4.7, then return the IDL interface type value that represents a reference to that user object.";
     QUnit.test(requirement, function(){
-      var userObj = Object.create({});
-      QUnit.strictEqual(window.WebIDL.Interface(userObj.constructor.prototype), Object.prototype, 'Object.create is valid user object.');
+      var userObj = Object.create({test: function(){ return false; }});
+      QUnit.strictEqual(window.WebIDL.Interface(userObj), Object, 'Object.create is valid user object.');
     });
 
 });
